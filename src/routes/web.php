4565f3a1101113a76b05e89784eab920deca0754
martijnr17 <44368317+martijnr17@users.noreply.github.com>
2023-11-09 10:14:57 +0100

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,8 +7,10 @@
 |
 */
 
-Route::group(['prefix' => 'verification', 'as' => 'laravel2step::', 'namespace' => 'jeremykenedy\laravel2step\App\Http\Controllers', 'middleware' => ['web']], function () {
-    Route::get('/needed', ['uses' => 'TwoStepController@showVerification'])->name('verificationNeeded');
-    Route::post('/verify', ['uses' => 'TwoStepController@verify'])->name('verify');
-    Route::post('/resend', ['uses' => 'TwoStepController@resend'])->name('resend');
-});
+Route::group(
+    ['prefix' => 'verification', 'as' => 'laravel2step::', 'namespace' => 'jeremykenedy\laravel2step\App\Http\Controllers', 'middleware' => ['web']], function () {
+        Route::get('/needed', ['uses' => 'TwoStepController@showVerification'])->name('verificationNeeded');
+        Route::post('/verify', ['uses' => 'TwoStepController@verify'])->name('verify');
+        Route::post('/resend', ['uses' => 'TwoStepController@resend'])->name('resend');
+    }
+);
